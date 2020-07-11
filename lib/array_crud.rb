ee567030a85eda5ef666a36f7bef288797868f7a
Adam Palmer <theadampalmer@gmail.com>
2020-07-11 00:25:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ end
 def add_element_to_end_of_array(array, element)
   
   array = ["wow", "I", "am", "really", "learning"]
-  array << "arrays"
+  array << "arrays!"
   puts array.inspect
   
 end
