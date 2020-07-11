ae50b23ce4d84c8d7d8cb59f77e4a4d9401e3297
Adam Palmer <theadampalmer@gmail.com>
2020-07-11 00:31:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,19 @@
 def create_an_empty_array
-  
   []
 end
 
 def create_an_array
-  
   puppies = ["bulldog", "golden", "pitbull", "lab"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
   array = ["wow", "I", "am", "really", "learning"]
   array << "arrays!"
-  
 end
 
 def add_element_to_start_of_array(array, element)
-  
   array = ["I", "am", "really", "learning"]
   array.unshift("wow")
-  
 end
 
 def remove_element_from_end_of_array(array)
